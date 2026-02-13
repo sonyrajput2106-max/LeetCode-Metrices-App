9124ce8e19a02f42d4b84846adb9f7e8d4bc83a5
sonyrajput2106-max <sonyrajput2106@gmail.com>
2026-02-13 23:22:50 +0530

    first commit

---
+++ LeetMetrices/script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const searchButton = document.getElementById("search-button");
+    const userNameInput = document.getElementById("user-input");
+    const starsContainer = document.querySelector(".stats-container");
+    const easyProgressCircle = document.querySelector(".easy-progress");
+    const mediumProgressCircle = document.querySelector(".medium-progress");
+    const hardProgressCircle = document.querySelector(".hard-progress");
+    const easyLabel = document.getElementById("easy-label");
+    const mediumLabel = document.getElementById("medium-label");
+    const hardLabel = document.getElementById("haed-label");
+
+
+})
